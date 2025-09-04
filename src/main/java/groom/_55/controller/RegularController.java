1223b9d4f9ee7baf67a5472dc4d2a4d7d091a1e1
KwonHarim <halim7753@gmail.com>
2025-09-04 21:25:26 +0900

    feat: JWT 리프레시 토큰 추가 및 스웨거 작성

--- src/main/java/groom/_55/controller/RegularController.java
+++ src/main/java/groom/_55/controller/RegularController.java
@@ -8,6 +8,8 @@ import groom._55.dto.response.CouponResponse;
 import groom._55.dto.response.MyPageResponse;
 import groom._55.dto.response.RegularMainResponse;
 import groom._55.service.RegularService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/regular")
+@Tag(name = "Regular(단골)", description = "단골 관련 API")
 @RequiredArgsConstructor // 의존성 주입을 위한 Lombok 어노테이션
 public class RegularController {
 
@@ -24,6 +27,7 @@ public class RegularController {
 
     // GET /regular/store/detail/{storeId}
     @GetMapping("/store/detail/{storeId}")
+    @Operation(summary = "가게 상세보기", description = "가게의 주소, 전화번호, 소개글 등이 포함되어 있는 가게의 자세 정보 페이지입니다. 가장 최근의 공지를 가져오고 읽음 여부는 hasNewNoti로 True/false로 구분합니다.")
     public ResponseEntity<RegularStoreDetail> getRegularStoreDetail(@PathVariable Long storeId, Authentication authentication) {
         // 실제 유저 ID는 세션, JWT 토큰 등에서 가져와야 합니다.
         Long userId = Long.parseLong(authentication.getName());
@@ -33,6 +37,7 @@ public class RegularController {
 
     // POST /regular/noti/read
     @PostMapping("/noti/read")
+    @Operation(summary="공지 읽음 버튼", description = "전송 시 해당 공지를 읽음 처리합니다.")
     public ResponseEntity<String> markNotiAsRead(@RequestBody NotiReadRequest request, Authentication authentication) {
         Long userId = Long.parseLong(authentication.getName());
         regularService.readNoti(userId, request.getNotiId());
@@ -43,6 +48,7 @@ public class RegularController {
     //    해당 함수는 만약 Stamp 데이터베이스에 등록이 안되어있다면 오류 발생
     //    즉 만약 단골 등록이 안되어있는 상태로 스탬프 찍으면 서버 에러 발생
     @PostMapping("/store/detail")
+    @Operation(summary = "해당 가게의 스탬프 찍기 버튼입니다.")
     public ResponseEntity<String> registerRegularStore(@RequestBody StampRequest request, Authentication authentication) {
         // userId와 storeId를 요청 본문에서 직접 가져옵니다.
         Long storeId = request.getStoreId();
@@ -54,6 +60,7 @@ public class RegularController {
     }
 
     @GetMapping("/main")
+    @Operation(summary = "단골 메인홈 페이지 입니다.", description = "방문한지 오래된 순서로 전달합니다.")
     public ResponseEntity<List<RegularMainResponse>> getRegularMain(Authentication authentication) {
         Long userId = Long.parseLong(authentication.getName());
         List<RegularMainResponse> response = regularService.getRegularStores(userId);
@@ -61,6 +68,7 @@ public class RegularController {
     }
 
     @GetMapping("/coupon")
+    @Operation(summary = "쿠폰함 페이지 입니다.", description = "쿠폰이 몇개 있는지 확인 가능합니다.")
     public ResponseEntity<List<CouponResponse>> getCoupons(Authentication authentication) {
         Long userId = Long.parseLong(authentication.getName());
         List<CouponResponse> response = regularService.getCoupons(userId);
@@ -68,6 +76,7 @@ public class RegularController {
     }
 
     @PostMapping("/coupon/{stampId}")
+    @Operation(summary = "쿠폰함 사용 버튼입니다.")
     public ResponseEntity<String> useCoupon(@PathVariable Long stampId, Authentication authentication) {
         Long userId = Long.parseLong(authentication.getName());
         regularService.useCoupon(userId, stampId);
@@ -75,6 +84,7 @@ public class RegularController {
     }
 
     @GetMapping("/mypage")
+    @Operation(summary = "나의 정보들을 혹인할 수 있는 페이지입니다.")
     public ResponseEntity<MyPageResponse> getMyPage(Authentication authentication) {
         Long userId = Long.parseLong(authentication.getName());
         MyPageResponse response = regularService.getMyPage(userId);
