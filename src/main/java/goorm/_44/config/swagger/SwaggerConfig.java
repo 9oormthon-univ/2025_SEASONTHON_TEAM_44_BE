20c7273814979bd04c8157d8158524b97517d912
zoo0o <qmdlxlr@gmail.com>
2025-09-06 20:09:04 +0900

    fix: swagger 경로 설정

--- src/main/java/goorm/_44/config/swagger/SwaggerConfig.java
+++ src/main/java/goorm/_44/config/swagger/SwaggerConfig.java
@@ -33,9 +33,6 @@ public class SwaggerConfig {
                         .title("다시온 API")
                         .description("[2025 kakao X groom 시즌톤] 44팀 다시온 BE")
                         .version("v1.0.0"))
-                .servers(List.of(
-                        new Server().url("http://localhost:8080").description("Local Server")
-                ))
                 // Components에 보안 스키마를 추가
                 .components(new Components().addSecuritySchemes(jwtSchemeName, securityScheme))
                 // 전체 API에 보안 요구사항을 적용
