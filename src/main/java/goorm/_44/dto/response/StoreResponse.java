b911afc3c6bc983259f624c50531af7a9c42641d
zoo0o <qmdlxlr@gmail.com>
2025-09-17 01:38:59 +0900

    feat: 내 가게 조회 메뉴이미지 필드 추가 (#52)

--- src/main/java/goorm/_44/dto/response/StoreResponse.java
+++ src/main/java/goorm/_44/dto/response/StoreResponse.java
@@ -1,14 +1,17 @@
 package goorm._44.dto.response;
 
+import java.util.List;
+
 public record StoreResponse(
         Long id,
         String name,
-        String imageUrl,
+        String storeImageUrl,
         String introduction,
         String phone,
         String address,
         String detailAddress,
         String open,
         String close,
-        String category
+        String category,
+        List<String> menuImageUrls
 ) {}
